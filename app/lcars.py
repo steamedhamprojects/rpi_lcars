5f857d7aacc122c7864f54e2e72f56b804268ae5
Kim Rypstra <kim.rypstra@gmail.com>
2021-02-13 14:59:38 +0800

    tabs/spaces

--- app/lcars.py
+++ app/lcars.py
@@ -5,8 +5,8 @@ import serial
 
 if __name__ == "__main__":
 
-	ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-	ser.flush()
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.flush()
 
     firstScreen = TestScreen()
     ui = UserInterface(firstScreen, config.RESOLUTION, config.UI_PLACEMENT_MODE, config.FPS, config.DEV_MODE,
@@ -14,7 +14,6 @@ if __name__ == "__main__":
 
     while (True):
         if ser.in_waiting > 0:
-			line = ser.readline().decode('utf-8').rstrip()
-			print(line)
-
-		ui.tick()
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+            ui.tick()
