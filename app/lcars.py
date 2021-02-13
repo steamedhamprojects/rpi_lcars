c98ff426cfaecbbe23c7500db08e5a4b30ec659f
Kim Rypstra <kim.rypstra@gmail.com>
2021-02-13 14:55:10 +0800

    Added serial reader to lcars.py

--- app/lcars.py
+++ app/lcars.py
@@ -1,11 +1,20 @@
 from screens.test import TestScreen
 from ui.ui import UserInterface
 import config
+import serial
 
 if __name__ == "__main__":
+
+	ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+	ser.flush()
+
     firstScreen = TestScreen()
     ui = UserInterface(firstScreen, config.RESOLUTION, config.UI_PLACEMENT_MODE, config.FPS, config.DEV_MODE,
                        config.SOUND)
 
     while (True):
-        ui.tick()
+        if ser.in_waiting > 0:
+			line = ser.readline().decode('utf-8').rstrip()
+			print(line)
+
+		ui.tick()
